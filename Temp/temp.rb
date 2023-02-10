575e464de47621618cd25e5a04786963dcc4839e
Michelle_Mungai <chellymungai@gmail.com>
2023-02-10 09:56:18 +0300

    Completed

---
+++ Temp/temp.rb
@@ -0,0 +1,21 @@
+def convert_temp(temp, input_scale: 'celsius', output_scale: 'celsius')
+
+    arr = [input_scale, output_scale]
+
+    case arr
+        when ['celsius', 'celsius']
+        return temp
+        when ['celsius', 'fahrenheit']
+        return ((temp * 9) / 5) + 32.0
+        when ['celsius', 'kelvin']
+        return temp + 273.15
+        when ['fahrenheit', 'celsius']
+        return ((temp - 32.0) * 5) / 9
+        when ['kelvin', 'celsius']
+        return temp - 273.15
+        when ['fahrenheit', 'kelvin']
+        return (((temp - 32.0) * 5) / 9) + 273.15
+        when ['kelvin', 'fahrenheit']
+        return (((temp - 273.15) * 9) / 5) + 32.0
+    end
+end
